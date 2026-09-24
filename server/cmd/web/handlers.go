e656032135d3d5a5e788c3b64495f7a37a46720a
Keiran Barnes <ckeiranbarnes@gmail.com>
2026-09-24 17:17:17 +0100

    Working insert to database

--- server/cmd/web/handlers.go
+++ server/cmd/web/handlers.go
@@ -2,12 +2,33 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/uuid"
 	"net/http"
 	"strconv"
 )
 
 func (app *application) bookCreate(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Server", "Go")
+
+	id := uuid.New()
+	idBytes, err := id.MarshalBinary()
+	if err != nil {
+		panic(err)
+	}
+
+	title := "go title"
+	author := "go author"
+	isbn := "go isbn"
+	noOfPages := 67
+	publisher := "go publisher"
+	review := "go review"
+
+	_, err = app.read.Insert(idBytes, title, author, isbn, noOfPages, publisher, review)
+	if err != nil {
+		app.serverError(w, r, err)
+		return
+	}
+
 	w.Write([]byte("Hello from server"))
 }
 
--- server/internal/models/books.go
+++ server/internal/models/books.go
@@ -2,11 +2,12 @@ package models
 
 import (
 	"database/sql"
-	"uuid"
+
+	"github.com/google/uuid"
 )
 
 type Book struct {
-	UUIDBin   uuid.UUID
+	UUIDBin   []byte
 	Title     string
 	Author    string
 	ISBN      string
@@ -20,15 +21,31 @@ type ReadModel struct {
 }
 
 func (m *ReadModel) Insert(
-	id_bin uuid.UUID,
+	idBytes []byte,
 	title string,
 	author string,
 	isbn string,
-	noOfPages string,
+	noOfPages int,
 	publisher string,
 	review string,
 ) (int, error) {
-	return 0, nil
+	stmt := `
+		INSERT INTO book (id_bin, title, author, isbn, no_of_pages, publisher, review) VALUES (
+			?, ?, ?, ?, ?, ?, ?
+		);
+	`
+
+	res, err := m.DB.Exec(stmt, idBytes, title, author, isbn, noOfPages, publisher, review)
+	if err != nil {
+		return 0, err
+	}
+
+	id, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return int(id), nil
 }
 
 func (m *ReadModel) Get(id uuid.UUID) (Book, error) {
