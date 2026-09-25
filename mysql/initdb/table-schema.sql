f275f0668313065838c6f1348c681168bef8d65e
Keiran Barnes <ckeiranbarnes@gmail.com>
2026-09-25 12:04:19 +0100

    docker compose files

---
+++ mysql/initdb/table-schema.sql
@@ -0,0 +1,11 @@
+USE readstore;
+CREATE TABLE book(
+  id_bin BINARY(16) NOT NULL PRIMARY KEY,
+  id_text VARCHAR (36) GENERATED ALWAYS AS (bin_to_uuid(id_bin)) VIRTUAL,
+  title VARCHAR(255),
+  author VARCHAR(255),
+  isbn VARCHAR(17),
+  no_of_pages INT,
+  publisher VARCHAR(255),
+  review TEXT
+);
